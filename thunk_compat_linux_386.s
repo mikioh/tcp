dc73afbf58f07ab782fceac69323843467279c3d
Mikio Hara <mikioh.mikioh@gmail.com>
2014-10-01 19:57:54 +0900

    tcp: consolidate syscall wrapper for linux/386

--- thunk_compat_linux_386.s
+++
@@ -1,8 +0,0 @@
-// Copyright 2014 Mikio Hara. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// +build go1.2,!go1.4
-
-TEXT	·socketcall(SB),4,$0-40
-	JMP	syscall·socketcall(SB)
