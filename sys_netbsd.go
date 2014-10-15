67e11a9524d5d96f56d3244293485cbabcc41a77
Mikio Hara <mikioh.mikioh@gmail.com>
2014-10-15 23:18:34 +0900

    tcp: drop unnecessary comment

--- sys_netbsd.go
+++ sys_netbsd.go
@@ -16,5 +16,4 @@ var sockOpts = [ssoMax]sockOpt{
 	ssoKeepAliveIdleInterval:  {sysTCP_KEEPIDLE, ssoTypeInt, time.Second},
 	ssoKeepAliveProbeInterval: {sysTCP_KEEPINTVL, ssoTypeInt, time.Second},
 	ssoKeepAliveProbes:        {sysTCP_KEEPCNT, ssoTypeInt, 0},
-	//	ssoCork:                   {sysTCP_NOPUSH, ssoTypeInt, 0},
 }
