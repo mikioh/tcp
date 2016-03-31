83044f7763b7a2f2328d18e9c39a74f7296bc971
Mikio Hara <mikioh.mikioh@gmail.com>
2016-03-31 11:17:18 +0900

    tcp: add concurrent read/write and info test

---
+++ readwrite_test.go
@@ -0,0 +1,85 @@
+// Copyright 2016 Mikio Hara. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tcp_test
+
+import (
+	"net"
+	"sync"
+	"testing"
+
+	"github.com/mikioh/tcp"
+)
+
+func server(t *testing.T, ln net.Listener) {
+	c, err := ln.Accept()
+	if err != nil {
+		return
+	}
+	go server(t, ln)
+	defer c.Close()
+	var b [1]byte
+	if _, err := c.Read(b[:]); err != nil {
+		t.Error(err)
+		return
+	}
+	if _, err := c.Write(b[:]); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestConcurrentReadWriteAndInfo(t *testing.T) {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ln.Close()
+	go server(t, ln)
+
+	const N = 10
+	var wg sync.WaitGroup
+	wg.Add(N)
+	for i := 0; i < N; i++ {
+		go func() {
+			defer wg.Done()
+			var d net.Dialer
+			c, err := d.Dial(ln.Addr().Network(), ln.Addr().String())
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			defer c.Close()
+			tc, err := tcp.NewConn(c)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			var wwg sync.WaitGroup
+			wwg.Add(1)
+			go func() {
+				defer wwg.Done()
+				var b [1]byte
+				if _, err := c.Write(b[:]); err != nil {
+					t.Error(err)
+					return
+				}
+				if _, err := c.Read(b[:]); err != nil {
+					t.Error(err)
+					return
+				}
+			}()
+			wwg.Add(1)
+			go func() {
+				defer wwg.Done()
+				if _, err := tc.Info(); err != nil {
+					t.Error(err)
+					return
+				}
+			}()
+			wwg.Wait()
+		}()
+	}
+	wg.Wait()
+}
