b7351bde23974c1176646ce361e3208fc078d2bf
Mikio Hara <mikioh.mikioh@gmail.com>
2016-06-23 06:01:54 +0900

    tcp: fix build on linux/386

--- sys_linux_386.go
+++ sys_linux_386.go
@@ -13,16 +13,16 @@ const sysGETSOCKOPT = 0xf
 
 func socketcall(call int, a0, a1, a2, a3, a4, a5 uintptr) (int, syscall.Errno)
 
-func getsockopt(s, level, name int, v unsafe.Pointer, l *uint32) error {
-	if _, errno := socketcall(sysGETSOCKOPT, uintptr(s), uintptr(level), uintptr(name), uintptr(v), uintptr(unsafe.Pointer(l)), 0); errno != 0 {
+func getsockopt(s uintptr, level, name int, v unsafe.Pointer, l *uint32) error {
+	if _, errno := socketcall(sysGETSOCKOPT, s, uintptr(level), uintptr(name), uintptr(v), uintptr(unsafe.Pointer(l)), 0); errno != 0 {
 		return error(errno)
 	}
 	return nil
 }
 
-func ioctl(s, ioc int) (int, error) {
+func ioctl(s uintptr, ioc int) (int, error) {
 	var i int
-	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(s), uintptr(ioc), uintptr(unsafe.Pointer(&i))); errno != 0 {
+	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, s, uintptr(ioc), uintptr(unsafe.Pointer(&i))); errno != 0 {
 		return 0, error(errno)
 	}
 	return i, nil
