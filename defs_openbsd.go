e2fde87d7a661fde68fce1a60f48f59cfbde045c
Mikio Hara <mikioh.mikioh@gmail.com>
2016-11-23 09:33:38 +0900

    tcp: add support for OriginalDst method of Conn on OpenBSD

--- defs_openbsd.go
+++ defs_openbsd.go
@@ -6,11 +6,49 @@
 
 package tcp
 
+// +godefs map struct_in_addr [4]byte /* in_addr */
+// +godefs map struct_in6_addr [16]byte /* in6_addr */
+// +godefs map struct_pf_addr [16]byte /* pf_addr */
+
 /*
 #include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+#include <net/if.h>
+#include <net/pfvar.h>
 */
 import "C"
 
 const (
 	sysFIONREAD = C.FIONREAD
+
+	sysAF_INET  = C.AF_INET
+	sysAF_INET6 = C.AF_INET6
+
+	sysPF_INOUT = C.PF_INOUT
+	sysPF_IN    = C.PF_IN
+	sysPF_OUT   = C.PF_OUT
+	sysPF_FWD   = C.PF_FWD
+
+	sysDIOCNATLOOK = C.DIOCNATLOOK
+)
+
+type sockaddrStorage C.struct_sockaddr_storage
+
+type sockaddr C.struct_sockaddr
+
+type sockaddrInet C.struct_sockaddr_in
+
+type sockaddrInet6 C.struct_sockaddr_in6
+
+type pfiocNatlook C.struct_pfioc_natlook
+
+const (
+	sizeofSockaddrStorage = C.sizeof_struct_sockaddr_storage
+	sizeofSockaddr        = C.sizeof_struct_sockaddr
+	sizeofSockaddrInet    = C.sizeof_struct_sockaddr_in
+	sizeofSockaddrInet6   = C.sizeof_struct_sockaddr_in6
+	sizeofPfiocNatlook    = C.sizeof_struct_pfioc_natlook
 )
