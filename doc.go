d68813b7bd5fff950bdcf93d9cdb80da6c699b9e
Mikio Hara <mikioh.mikioh@gmail.com>
2016-12-17 09:35:20 +0900

    tcp: update package documentation

--- doc.go
+++ doc.go
@@ -7,16 +7,6 @@
 // The package provides TCP-level socket options that allow
 // manipulation of TCP connection facilities.
 //
-// The Transmission Control Protocol (TCP) is defined in RFC 793.
-// TCP Selective Acknowledgment Options is defined in RFC 2018.
-// Management Information Base for the Transmission Control Protocol
-// (TCP) is defined in RFC 4022.
-// TCP Congestion Control is defined in RFC 5681.
-// Computing TCP's Retransmission Timer is described in RFC 6298.
-// TCP Options and Maximum Segment Size (MSS) is defined in RFC 6691.
-// Shared Use of Experimental TCP Options is defined in RFC 6994.
-// TCP Extensions for High Performance is defined in RFC 7323.
-//
 //
 // Monitoring a TCP connection
 //
