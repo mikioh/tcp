f1d847c4c06bbfeabd47636817e2c8095f1340db
Mikio Hara <mikioh.mikioh@gmail.com>
2017-06-14 11:28:56 +0900

    tcp: avoid potential data race on go1.8 or below

--- reflect.go
+++ reflect.go
@@ -12,6 +12,8 @@ import (
 	"os"
 	"reflect"
 	"runtime"
+	"sync/atomic"
+	"syscall"
 
 	"github.com/mikioh/tcpopt"
 )
@@ -27,44 +29,29 @@ type Conn struct {
 func (c *Conn) ok() bool { return c != nil && c.Conn != nil }
 
 func (c *Conn) setOption(level, name int, b []byte) error {
-	s, err := socketOf(c.Conn)
-	if err != nil {
-		return err
-	}
-	return os.NewSyscallError("setsockopt", setsockopt(s, level, name, b))
+	return os.NewSyscallError("setsockopt", setsockopt(c.s, level, name, b))
 }
 
 func (c *Conn) option(level, name int, b []byte) (int, error) {
-	s, err := socketOf(c.Conn)
-	if err != nil {
-		return 0, err
-	}
-	n, err := getsockopt(s, level, name, b)
+	n, err := getsockopt(c.s, level, name, b)
 	return n, os.NewSyscallError("getsockopt", err)
 }
 
 func (c *Conn) buffered() int {
-	s, err := socketOf(c.Conn)
-	if err != nil {
-		return -1
-	}
 	var b [4]byte
-	if err := ioctl(s, options[soBuffered].name, b[:]); err != nil {
+	if err := ioctl(c.s, options[soBuffered].name, b[:]); err != nil {
 		return -1
 	}
 	return int(nativeEndian.Uint32(b[:]))
 }
 
 func (c *Conn) available() int {
-	s, err := socketOf(c.Conn)
-	if err != nil {
-		return -1
-	}
+	var err error
 	var b [4]byte
 	if runtime.GOOS == "darwin" {
-		_, err = getsockopt(s, options[soAvailable].level, options[soAvailable].name, b[:])
+		_, err = getsockopt(c.s, options[soAvailable].level, options[soAvailable].name, b[:])
 	} else {
-		err = ioctl(s, options[soAvailable].name, b[:])
+		err = ioctl(c.s, options[soAvailable].name, b[:])
 	}
 	if err != nil {
 		return -1
@@ -72,13 +59,33 @@ func (c *Conn) available() int {
 	n := int(nativeEndian.Uint32(b[:]))
 	if runtime.GOOS == "darwin" || runtime.GOOS == "linux" {
 		var o tcpopt.SendBuffer
-		_, err = getsockopt(s, o.Level(), o.Name(), b[:])
+		_, err = getsockopt(c.s, o.Level(), o.Name(), b[:])
 		if err != nil {
 			return -1
 		}
 		n = int(nativeEndian.Uint32(b[:])) - n
 	}
 	return n
+}
+
+// Close closes the connection.
+func (c *Conn) Close() error {
+	if !c.ok() {
+		return syscall.EINVAL
+	}
+	atomic.StoreUintptr(&c.s, ^uintptr(0))
+	return c.Conn.Close()
+}
+
+// NewConn returns a new end point.
+func NewConn(c net.Conn) (*Conn, error) {
+	s, err := socketOf(c)
+	if err != nil {
+		return nil, err
+	}
+	cc := &Conn{Conn: c}
+	atomic.StoreUintptr(&cc.s, s)
+	return cc, nil
 }
 
 func socketOf(c net.Conn) (uintptr, error) {
@@ -99,13 +106,4 @@ func socketOf(c net.Conn) (uintptr, error) {
 		}
 	}
 	return 0, errors.New("invalid type")
-}
-
-// NewConn returns a new end point.
-func NewConn(c net.Conn) (*Conn, error) {
-	s, err := socketOf(c)
-	if err != nil {
-		return nil, err
-	}
-	return &Conn{Conn: c, s: s}, nil
 }
